824c6997c89cf5cf1e79789d43bf07cb2a2f75ee
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-01-09 16:12:38 +0600

    add profile page

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from . models import Room, Topic
+from . models import Room, Topic, Message
 from . forms import RoomForm
 
 
@@ -77,19 +77,40 @@ def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
     rooms = Room.objects.filter(Q(topic__name__contains=q) | Q(name__icontains=q) | Q(description__icontains=q))
+
     room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics':topics, 'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
+
+
+    context = {'rooms': rooms, 'topics':topics, 'room_count':room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {'room': room}
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+    perticipants = room.perticipants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.perticipants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages':room_messages, 'perticipants':perticipants}
     return render(request, 'base/room.html', context)
 
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms':rooms, 'room_messages': room_messages, 'topics':topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -139,3 +160,16 @@ def deleteRoom(request, pk):
 
 
 
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message =  Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowrd here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete.html', {'obj':message})
