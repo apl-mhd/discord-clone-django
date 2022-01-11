7b573973b552dae47ee1dc297f96ac828b8c3df4
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-01-12 00:09:53 +0600

    mobile responsive done

--- base/urls.py
+++ base/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    path('topics/', views.topicsPage, name='topics'),
 
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('update-user/', views.updateUser, name='update-user'),
@@ -19,6 +20,7 @@ urlpatterns = [
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
-    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message")
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('activity/', views.activityPage, name="activity"),
 ]
 
--- base/views.py
+++ base/views.py
@@ -80,7 +80,7 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__contains=q) | Q(name__icontains=q) | Q(description__icontains=q))
 
     room_count = rooms.count()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
 
 
@@ -202,3 +202,14 @@ def updateUser(request):
 
     context ={'forms':forms}
     return render(request, 'base/update-user.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html',{'topics':topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages':room_messages})
